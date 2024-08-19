54b5f52040df1a1b06abc41f920e9a160314fcdc
caesar-chin <caesarc.1628@gmail.com>
2024-08-19 00:44:30 -0400

    new commit

--- helper/modify_string.js
+++ helper/modify_string.js
@@ -1,7 +1,9 @@
 //exmaple: modifies "Weyes Blood, Brooklyn Steel" into "weyes_blood_brooklyn_steel" by removing spaces and punctuation and adding underscores
+// comment
 exports.modify_string = (occasion_name) => {
   return occasion_name
     .toLowerCase()
     .replace(/[^a-zA-Z0-9]/g, "_")
     .replace(/_{2,}/g, "_");
 };
+
