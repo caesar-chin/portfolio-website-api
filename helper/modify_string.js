93ed310f55de042b4008041234f8e3157570015e
caesar-chin <caesarc.1628@gmail.com>
2024-09-11 00:29:10 -0400

    fixed remove all special character function

--- helper/modify_string.js
+++ helper/modify_string.js
@@ -1,21 +1,8 @@
 //exmaple: modifies "Weyes Blood, Brooklyn Steel" into "weyes_blood_brooklyn_steel" by removing spaces and punctuation and adding underscores
 // comment
-exports.modify_string = (arrayOfObjects) => {
-  return arrayOfObjects
-    .filter(obj => Object.keys(obj).length !== 0) // Remove empty objects
-    .map(obj => {
-      // Iterate over the keys of the object and modify each string value
-      let modifiedObj = {};
-      for (let key in obj) {
-        if (typeof obj[key] === "string") {
-          modifiedObj[key] = obj[key]
-            .toLowerCase()
-            .replace(/[^a-zA-Z0-9]/g, "_")  // Replace special characters with underscores
-            .replace(/_{2,}/g, "_");         // Replace multiple underscores with a single one
-        } else {
-          modifiedObj[key] = obj[key]; // If the value is not a string, keep it as is
-        }
-      }
-      return modifiedObj;
-    });
+exports.modify_string = (occasion_name) => {
+  return occasion_name
+    .toLowerCase()  // Convert the string to lowercase
+    .replace(/[^a-zA-Z0-9]+/g, "_")  // Replace one or more non-alphanumeric characters with a single underscore
+    .replace(/^_+|_+$/g, "");  // Remove leading and trailing underscores
 };
